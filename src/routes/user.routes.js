29aa25217c8fc92324b36821b4525506a29ad105
anmol420 <anmolanand2k15@gmail.com>
2024-10-06 21:58:23 +0530

    Added LogIn Data Validator

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { registerUser, logInUser } from "../controllers/user.controller.js";
 import validationSchema from "../middlewares/zodValidator.middleware.js";
-import { registerSchema } from "../utils/schemas/userValidator.js";
+import { registerSchema, logInSchema } from "../utils/schemas/userValidator.js";
 
 const router = Router();
 
 router.route("/register").post(validationSchema(registerSchema), registerUser);
-router.route("/login").post(logInUser);
+router.route("/login").post(validationSchema(logInSchema), logInUser);
 
 export default router;
--- src/utils/schemas/userValidator.js
+++ src/utils/schemas/userValidator.js
@@ -6,7 +6,12 @@ const registerSchema = z.object({
     password: z.string().min(5, { message: "Minimum Password Length Should Be 5 Characters." }),
 });
 
+const logInSchema = z.object({
+    email: z.string().email({ message: "Invalid Email Format" }),
+    password: z.string().min(5, { message: "Minimum Password Length Should Be 5 Characters." }),
+});
 
 export {
-    registerSchema
+    registerSchema,
+    logInSchema
 };
