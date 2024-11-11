2601dad1d12abc053651750c9556e7b2dad74bb2
anmol420 <anmolanand2k15@gmail.com>
2024-11-11 19:01:42 +0530

    Increased Hashing Round

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -29,7 +29,7 @@ userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) {
         return next();
     }
-    this.password = await bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 13);
     next();
 });
 
